a16f0ca73ffa96d244b4fa5620830dfdeb1de9a8
Alessandro Marrella <alessandro@bandstand.dev>
2022-02-13 15:45:00 +0100

    Init

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "NodeJS developer shell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    devshell.url = "github:numtide/devshell";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, flake-utils, devshell, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      devShell =
+        let pkgs = import nixpkgs {
+          inherit system;
+
+          overlays = [ devshell.overlay ];
+        };
+        in
+        pkgs.devshell.mkShell {
+          imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
+        };
+    });
+}
